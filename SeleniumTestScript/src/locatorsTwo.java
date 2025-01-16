a88ca7196cc4fe83ce9e630a5aaa4efad72d8a68
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2025-01-17 00:48:29 +0530

    Added Selenium script for locators and assertions

--- SeleniumTestScript/src/locatorsTwo.java
+++ SeleniumTestScript/src/locatorsTwo.java
@@ -9,11 +9,12 @@ import org.testng.Assert;
 public class locatorsTwo {
 
 	public static void main(String[] args) throws InterruptedException {
+		String name= "Shubham";
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.manage().window().maximize();
-		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
 		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
 	    driver.findElement(By.className("signInBtn")).click();
 	    Thread.sleep(3000);
@@ -21,6 +22,10 @@ public class locatorsTwo {
 	  System.out.println(actual);
 	  String expected = "You are successfully logged in.";
 	   Assert.assertEquals(actual, expected);
+	   String actual1 = driver.findElement(By.tagName("h2")).getText();
+	   String expected1 = "Hello "+name +",";
+	   System.out.println(expected1);
+	   Assert.assertEquals(actual1, expected1);
 	   driver.quit();
 
 	}
