c0189dbdb968379e72b6816b68aef8abbdaa533f
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2025-01-28 01:57:47 +0530

    Adding Selenium Code

--- SeleniumTestScript/src/GreenKart.java
+++ SeleniumTestScript/src/GreenKart.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -5,16 +6,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class GreenKart {
+	int j=0;
+	String[] itemRequired = {"Brocolli","Cucumber","Beetroot"};
 
 	public static void main(String[] args) throws InterruptedException {
-		int j=0;
-		String[] itemRequired = {"Brocolli","Cucumber","Beetroot"};
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
+		GreenKart gt = new GreenKart();
+		gt.selectItem(driver);
+
+		}
+	
+	public void selectItem(WebDriver driver) throws InterruptedException {
+		
 		List<WebElement> itemAvailable = driver.findElements(By.cssSelector("h4.product-name"));
 		int size = itemAvailable.size();
 		System.out.println(size);
@@ -37,5 +50,25 @@ public class GreenKart {
 		
 
 	}
+		driver.findElement(By.cssSelector("a.cart-icon")).click();
+		driver.findElement(By.xpath("//div[@class='action-block']/button[text()='PROCEED TO CHECKOUT']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+		
+		String actual = driver.findElement(By.cssSelector("span.promoInfo")).getText();
+		String expected = "Code applied ..!";
+		Assert.assertEquals(actual, expected);
+		driver.findElement(By.xpath("//*[text()='Place Order']")).click();
+		
+		Select dropdown = new Select(driver.findElement(By.xpath("//div[@class='products']/div/div/select")));
+		dropdown.selectByIndex(2);
+		driver.findElement(By.cssSelector("input.chkAgree")).click();
+		driver.findElement(By.xpath("//*[text()='Proceed']")).click();
+		driver.quit();
 	}
+	
 }
