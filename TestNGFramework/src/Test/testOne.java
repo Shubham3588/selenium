bd5c6b3d81b5039085485bfbfeab4cce440b9889
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2025-03-17 00:16:56 +0530

    Added TestNg Framework

--- TestNGFramework/src/Test/testOne.java
+++ TestNGFramework/src/Test/testOne.java
@@ -1,11 +1,13 @@
 package Test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class testOne {
 	@Test(groups= {"smoke"})
 	public void test() {
 		System.out.println("Hello");
+		Assert.assertTrue(false);
 	}
 
 }
