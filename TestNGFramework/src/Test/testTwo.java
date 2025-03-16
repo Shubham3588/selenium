79ad802e4d5b482deeec53e0d31bbad9bdd6f414
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2025-03-16 23:38:15 +0530

    Added TestNg Framework

--- TestNGFramework/src/Test/testTwo.java
+++ TestNGFramework/src/Test/testTwo.java
@@ -1,5 +1,6 @@
 package Test;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class testTwo {
@@ -15,6 +16,17 @@ public class testTwo {
 	@Test(dependsOnMethods= {"carLoan"})
 	public void adependentTest() {
 		System.out.println("I am dependent on car loan");
+	}
+	
+	@DataProvider
+	public void getData() {
+		Object[][] data = new Object[3][2];
+		data[0][0]= "un1";
+		data[0][1]="pwd1";
+		data[1][0]= "un2";
+		data[1][1]="pwd2";
+		data[2][0]= "un3";
+		data[2][1]="pwd3";
 	}
 
 
